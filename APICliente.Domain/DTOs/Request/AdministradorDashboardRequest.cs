20d029ba1897f3ba22b9ca92c35e157682f1c2f1
rafael chaves <rafaelaugustochaves@hotmail.com>
2022-03-15 13:54:39 -0300

    Classes para tranferencia de dados

---
+++ APICliente.Domain/DTOs/Request/AdministradorDashboardRequest.cs
@@ -0,0 +1,16 @@
+﻿using APICliente.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APICliente.Domain.DTOs.Request
+{
+    public class AdministradorDashboardRequest
+    {
+        public List<VendasCliente> VendasClientes { get; set; }
+        public List<Clientes> Clientes { get; set; }
+        public List<Despesas> Despesas { get; set; }
+    }
+}
---
+++ APICliente.Domain/DTOs/Request/Clientes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APICliente.Domain.DTOs.Request
+{
+    public class Clientes
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+    }
+}
